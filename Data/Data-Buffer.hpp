1126052835ecebe1125a65eeaa597cddad04d9eb
Gregory Potter <gjp.creates@gmail.com>
2025-07-11 16:48:57 -0400

    Update Data-Buffer.hpp
    Whitespace cleanup

--- Data/Data-Buffer.hpp
+++ Data/Data-Buffer.hpp
@@ -43,12 +43,12 @@ public:
 	Buffer(const Buffer& other)
 	: size_(other.size_)
 	{
-	  data_ = new float[size_];
+		data_ = new float[size_];
 		std::copy(other.data_, other.data_ + other.size_, data_);
 	}
 	Buffer& operator=(const Buffer& other) {
-	  size_ = other.size_;
-	  data_ = new float[size_];
+		size_ = other.size_;
+		data_ = new float[size_];
 		std::copy(other.data_, other.data_ + other.size_, data_);
 		return *this;
 	}
@@ -58,11 +58,11 @@ public:
 	: data_(other.data_)
 	, size_(other.size_)
 	{
-	  other.data_ = nullptr;
+		other.data_ = nullptr;
 		other.size_ = 0;
 	}
 	Buffer& operator=(Buffer&& other) {
-	  if (this != &other) {
+		if (this != &other) {
 			delete[] data_;
 			data_ = other.data_;
 			size_ = other.size_;
@@ -83,19 +83,19 @@ public:
 	size_t size() const noexcept {
 		return size_;
 	}
-
+	
 	float* begin() {
-	 return data_;
+		return data_;
 	}
 	
 	float* end() {
-	 return data_ + size_;
+		return data_ + size_;
 	}
 	
 	void fill(float value) {
 		std::fill(data_, data_ + size_, value);
 	}
-
+	
 	float rms() {
 		float sumOfSquares = 0.0f;
 		for (float& sample : *this) {
@@ -103,43 +103,43 @@ public:
 		}
 		return std::sqrt(sumOfSquares / size_);
 	}
-
+	
 	enum class Window {
 		Boxcar,
 		Hann,
 		Hamming,
 		Blackman
 	};
-
+	
 	void window(Window w) {
-  switch (w) {
-		case Window::Boxcar: {
-			break;
+		switch (w) {
+			case Window::Boxcar: {
+				break;
+			}
+			case Window::Hann: {
+				for (size_t i = 0; i < size_; i++) {
+					data_[i] *= 0.5 * (1.0 - cos(2.0 * M_PI * i / (size_ - 1)));
+				}
+				break;
+			}
+			case Window::Hamming: {
+				constexpr float a = 25.0 / 46.0;
+				for (size_t i = 0; i < size_; i++) {
+					data_[i] *= a - ((1.0 - a) * cos(2.0 * M_PI * i / (size_ - 1)));
+				}
+				break;
+			}
+			case Window::Blackman: {
+				constexpr float a0 = 7938.0 / 18608.0;
+				constexpr float a1 = 9240.0 / 18608.0;
+				constexpr float a2 = 1430.0 / 18608.0;
+				for (size_t i = 0; i < size_; i++) {
+					data_[i] *= a0 - (a1 * cos(2.0 * M_PI * i / (size_ - 1))) + (a2 * cos(4.0 * M_PI * i / (size_ - 1)));
+				}
+				break;
+			}
 		}
-    case Window::Hann: {
-      for (size_t i = 0; i < size_; i++) {
-        data_[i] *= 0.5 * (1.0 - cos(2.0 * M_PI * i / (size_ - 1)));
-      }
-      break;
-    }
-    case Window::Hamming: {
-      constexpr float a = 25.0 / 46.0;
-      for (size_t i = 0; i < size_; i++) {
-        data_[i] *= a - ((1.0 - a) * cos(2.0 * M_PI * i / (size_ - 1)));
-      }
-      break;
-    }
-    case Window::Blackman: {
-      constexpr float a0 = 7938.0 / 18608.0;
-      constexpr float a1 = 9240.0 / 18608.0;
-      constexpr float a2 = 1430.0 / 18608.0;
-      for (size_t i = 0; i < size_; i++) {
-        data_[i] *= a0 - (a1 * cos(2.0 * M_PI * i / (size_ - 1))) + (a2 * cos(4.0 * M_PI * i / (size_ - 1)));
-      }
-      break;
-    }
-  }
-}
+	}
 	
 private:
 	size_t size_;
